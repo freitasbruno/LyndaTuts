9b906c4cbcedbb5de93472a39aba66b72da98825
Bruno Freitas <freitascbruno@gmail.com>
2016-04-16 09:39:46 +0100

    A few more examples added

---
+++ ajax/006/script.js
@@ -0,0 +1,16 @@
+var request;
+if (window.XMLHttpRequest) {
+	request = new XMLHttpRequest();
+} else {
+	request = new ActiveXObject('Microsoft.XMLHTTP');
+}
+request.open('GET', 'data2.json');
+
+request.onreadystatechange = function() {
+	if ((request.readyState===4) && (request.status===200)){
+		var items = JSON.parse(request.responseText);
+		console.log(items);
+	}
+}
+
+request.send();
---
+++ ajax/008/script.js
@@ -0,0 +1,24 @@
+function loadAjax(){
+	var request;
+	if (window.XMLHttpRequest) {
+		request = new XMLHttpRequest();
+	} else {
+		request = new ActiveXObject('Microsoft.XMLHTTP');
+	}
+	request.open('GET', 'data.json');
+
+	request.onreadystatechange = function() {
+		if ((request.readyState===4) && (request.status===200)){
+			var items = JSON.parse(request.responseText);
+			var output = '<ul>';
+			for (var key in items) {
+					output += '<li>' + items[key].colorName + '</li>';
+			}
+			output += '</ul>';
+			document.getElementById('update').innerHTML = output;
+			console.log(items);
+		}
+	}
+
+	request.send();
+}
